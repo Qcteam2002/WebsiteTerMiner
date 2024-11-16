f6b4c056489973bcdc81451eff7b72bca830ab93
qcteam2002 <letuanh90@gmail.com>
2024-11-16 23:05:09 +0700

    Bô sung page Private Policy

--- script.js
+++ script.js
@@ -25,3 +25,20 @@ function toggleMenu() {
     menu.classList.toggle('open');
 }
 
+// Load Header
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));
+
+// Load Footer
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading footer:', error));
+
+
